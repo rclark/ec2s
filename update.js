6742261dd7260b3891f9c630765494dc9fc7a820
Ryan Clark <ryan.clark.j@gmail.com>
2017-06-24 23:34:04 -0700

    spot stability info

--- update.js
+++ update.js
@@ -3,6 +3,11 @@
 const fs = require('fs');
 const path = require('path');
 const got = require('got');
+const AWS = require('aws-sdk');
+const ss = require('simple-statistics');
+const Queue = require('p-queue');
+const Spinner = require('cli-spinner').Spinner;
+Spinner.setDefaultSpinnerString('⠄⠆⠇⠋⠙⠸⠰⠠⠰⠸⠙⠋⠇⠆');
 
 const regions = {
   'US East (N. Virginia)': 'us-east-1',
@@ -103,9 +108,119 @@ const extractData = (priceData) => {
   return sorted;
 };
 
-got.get('https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonEC2/current/index.json')
-  .then((response) => JSON.parse(response.body))
-  .then((data) => extractData(data))
+const spot = (type, region, onDemandPrice) => {
+  const ec2 = new AWS.EC2({ region });
+
+  return new Promise((resolve, reject) => {
+    const params = {
+      InstanceTypes: [type],
+      StartTime: new Date(Date.now() - 7 * 24 * 60 * 60 * 1000).toISOString(),
+      Filters: [
+        {
+          Name: 'product-description',
+          Values: ['Linux/UNIX']
+        }
+      ]
+    };
+
+    const reduce = (data) => {
+      const azData = data.reduce((azData, point) => {
+        const az = point.AvailabilityZone.split('-').pop();
+        azData[az] = azData[az] || [];
+        azData[az].push(Number(point.SpotPrice));
+        azData.overall.push(Number(point.SpotPrice));
+        return azData;
+      }, { overall: [] });
+
+      const azStats = Object.keys(azData).reduce((azStats, az) => {
+        azStats[az] = azData[az].length ? {
+          min: ss.min(azData[az]),
+          max: ss.max(azData[az]),
+          mean: Number(ss.mean(azData[az]).toFixed(6)),
+          variance: Number(ss.variance(azData[az]).toFixed(6)),
+          over: azData[az].filter((price) => price >= onDemandPrice).length,
+          count: azData[az].length
+        } : {
+          min: null,
+          max: null,
+          mean: null,
+          variance: null,
+          over: null,
+          count: 0
+        };
+        return azStats;
+      }, { type, region });
+
+      return azStats;
+    };
+
+    let priceData = [];
+
+    ec2.describeSpotPriceHistory(params).eachPage((err, data, done) => {
+      if (err) return reject(err);
+      if (!data) return resolve(reduce(priceData));
+      priceData = priceData.concat(data.SpotPriceHistory);
+      done();
+    });
+  });
+};
+
+const addSpotData = (data) => {
+  const spinner = new Spinner();
+  const queue = new Queue({ concurrency: 20 });
+  const requests = [];
+  let completed = 0;
+  let total = 0;
+
+  Object.keys(data).forEach((type) => {
+    Object.keys((data[type].price)).forEach((region) => {
+      if (region === 'us-gov-west-1') return;
+
+      requests.push(
+        queue.add(
+          () => spot(type, region, data[type].price[region]).then((data) => {
+            completed++;
+            spinner.setSpinnerTitle(`Analyzing spot history: ${(100 * (completed / total)).toFixed(2)}% complete`);
+            return data;
+          })
+        )
+      );
+    });
+  });
+
+  total = requests.length;
+  spinner.setSpinnerTitle(`Analyzing spot history: ${(100 * (completed / total)).toFixed(2)}% complete`);
+  spinner.start();
+
+  return Promise.all(requests).then((results) => {
+    results.forEach((spot) => {
+      const type = spot.type;
+      const region = spot.region;
+      data[type].spot = data[type].spot || {};
+      data[type].spot[region] = JSON.parse(JSON.stringify(spot));
+      delete data[type].spot[region].type;
+      delete data[type].spot[region].region;
+    });
+
+    spinner.stop(true);
+    return data;
+  });
+};
+
+Promise.resolve()
+  .then(() => {
+    const spinner = new Spinner('Fetching on-demand price data...');
+    spinner.start();
+
+    return got.get('https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonEC2/current/index.json')
+      .then((response) => JSON.parse(response.body))
+      .then((data) => extractData(data))
+      .then((data) => {
+        spinner.stop(true);
+        return data;
+      });
+  })
+  .then((data) => addSpotData(data))
   .then((data) => {
     const families = {};
 
@@ -127,4 +242,5 @@ got.get('https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonEC2/curre
         });
       fs.writeFile(path.join(__dirname, `${family}.json`), JSON.stringify(data, null, 2));
     });
-  });
+  })
+  .catch((err) => console.error(err.stack));
